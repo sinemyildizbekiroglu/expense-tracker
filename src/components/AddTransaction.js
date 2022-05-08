35c3c79df31e97b6b8d5915dc4100503cdbad1c9
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-05-08 19:41:52 +0200

    Add `AddTransaction.js`

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+export const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form>
+        <div className="form-control">
+          <label htmlfor="text">Text</label>
+          <input type="text" placeholder="Enter text..." />
+        </div>
+        <div className="form-control">
+          <label htmlfor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input type="number" placeholder="Enter amount..." />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  );
+};
